e93555cfa80c1678d33da4ab021d1b120fb1b6b0
Tadas <lauokal@gmail.com>
2022-10-03 21:54:08 +0300

    Complete tasks
    Complete tasks

---
+++ ReizTechTask/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Please insert hours (0 - 12):");
+int hours = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine("Please insert minutes (0 - 60):");
+int minutes = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine("Angle between hour and minute arrows: {0} degrees", CalculateAngle(hours, minutes));
+
+int CalculateAngle(int hours, int minutes)
+{
+    if(hours < 0 || minutes < 0 || hours > 12 || minutes > 60)
+    {
+        Console.WriteLine("Wrong input");
+        return 0;
+    }
+
+    hours = hours == 12 ? 0 : hours;
+
+    if(minutes == 60)
+    {
+        minutes = 0;
+        hours += 1;
+        hours = hours > 12 ? hours - 12 : hours;
+    }
+
+    int hour_angle = (int)(0.5 * (hours * 60 + minutes));
+    int minute_angle = (int)(6 * minutes);
+
+    int angle = Math.Abs(hour_angle - minute_angle);
+
+    angle = Math.Min(360 - angle, angle);
+
+    return angle;
+}
---
+++ ReizTechTask_2/Program.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+
+//Creating exact tree from example
+Branch<int> tree = new Branch<int>(1)
+{
+            new Branch<int>(2)
+            { 
+                new Branch<int>(3),
+            new Branch<int>(5)
+            {
+                new Branch<int>(5)
+                {
+                    new Branch<int>(4)  
+                },
+                new Branch<int>(6)
+                {
+                    new Branch<int>(4)
+                    {
+                        new Branch<int>(8)
+                    },
+                    new Branch<int>(7)
+                },
+                new Branch<int>(7)
+            },
+            }
+        };
+
+Console.WriteLine("Maximum depth of tree: {0}", FindDepthRecursively(tree));
+
+int FindDepthRecursively(Branch<int> tree)
+{
+    if(!tree.Any())
+    {
+        return 1;
+    }
+
+    int maxDepth = 0;
+
+    foreach (var branch in tree.branches)
+    {
+        maxDepth = Math.Max(maxDepth, FindDepthRecursively(branch));
+    }
+
+    return maxDepth + 1;
+}
+
+public class Branch<T> : IReadOnlyList<Branch<T>>
+{
+    public readonly List<Branch<T>> branches = new List<Branch<T>>();
+
+    public Branch(T? value)
+    {
+        Value = value;
+    }
+
+    public Branch() : this(default(T)) { }
+
+    public T? Value { get; set; }
+
+    public Branch<T>? Parent { get; private set; }
+
+    public void Add(Branch<T> child)
+    {
+        if (child is null)
+            throw new ArgumentNullException(nameof(child));
+
+        for (var parent = Parent; parent != null; parent = parent.Parent)
+            if (parent == child)
+                throw new ArgumentException("Trying to create a loop", nameof(child));
+
+        if (child.Parent is not null)
+            child.Parent.Remove(child);
+
+        branches.Add(child);
+
+        child.Parent = this;
+    }
+
+    public bool Remove(Branch<T> child)
+    {
+        if (child is null || !ReferenceEquals(child.Parent, this))
+            return false;
+
+        branches.Remove(child);
+        child.Parent = null;
+        return true;
+    }
+
+    public int Count => branches.Count;
+
+    public Branch<T> this[int index] => branches[index];
+
+    public IEnumerator<Branch<T>> GetEnumerator() => branches.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => branches.GetEnumerator();
+}
